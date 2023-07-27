25fd74c2dad81dcb6ce76bd865309b4274109386
Deniz Tezcan <10155092+deniztezcan@users.noreply.github.com>
2023-07-27 13:40:14 +0200

    skeleton

---
+++ src/Support/ServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Varenyky\Support;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }   
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/../../routes/cms.php');
+        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'varenyky');
+    }
+}
