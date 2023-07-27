52831e86679401bbe719d6df031cda6286d03174
Deniz Tezcan <10155092+deniztezcan@users.noreply.github.com>
2023-07-27 13:49:03 +0200

    base migrations

---
+++ database/migrations/2023_07_27_000000_create_pages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->longText('template')->nullable();
+            $table->longText('content');
+            $table->longText('seo_title')->nullable();
+            $table->longText('seo_desc')->nullable();
+            $table->longText('seo_snip')->nullable();
+            $table->longText('seo_key')->nullable();
+            $table->integer('parent')->nullable();
+            $table->tinyInteger('published')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+};
---
+++ database/migrations/2023_07_27_134500_create_menus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained();
+            $table->enum('type', ['cms', 'link']);
+            $table->string('name');
+            $table->longText('link')->nullable();
+            $table->integer('sort_order');
+            $table->integer('parent')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_items');
+    }
+};
