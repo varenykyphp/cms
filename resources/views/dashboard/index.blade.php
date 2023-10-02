8596a1cd30b7a94e19ac2a6548cca444e3e43214
Deniz Tezcan <10155092+deniztezcan@users.noreply.github.com>
2023-10-02 10:02:40 +0200

    dashb

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,81 @@
 @extends('varenykyAdmin::app')
 
 @section('content')
-    <div class="card border p-3">
-        <h3>Coming soon</h3>
-    </div>
+    @if ($isShop)
+        <div class="card p-3">
+            <div class="row">
+                <div class="col-6">
+                    <div class="border p-4">
+                        <h2 class="text-success">Hi {{ auth()->user()->name }}!</h2>
+                        <p class="lead">This is what happened with
+                            {{ \Varenyky\Models\Setting\Setting::where('key', 'site-name')->first()->value }} this week.</p>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="border p-4">
+                                <small>Orders</small>
+                                <h1 class="mb-0 mt-2">{{ $orders }}</h1>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <small class="text-muted">{{ $oldOrders }}</small>
+                                    </div>
+                                    <div class="col-6 text-end">
+                                        @if ($orderpercentage > 0)
+                                            <small class="text-success">{{ number_format($orderpercentage, 2) }}%</small>
+                                        @elseif($orderpercentage < 0)
+                                            <small class="text-danger">{{ number_format($orderpercentage, 2) }}%</small>
+                                        @else
+                                            <small class="text-muted">{{ number_format($orderpercentage, 2) }}%</small>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="border p-4">
+                                <small>New products</small>
+                                <h1 class="mb-0 mt-2">{{ $products }}</h1>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <small class="text-muted">{{ $oldProducts }}</small>
+                                    </div>
+                                    <div class="col-6 text-end">
+                                        @if ($productpercentage > 0)
+                                            <small class="text-success">{{ number_format($productpercentage, 2) }}%</small>
+                                        @elseif($productpercentage < 0)
+                                            <small class="text-danger">{{ number_format($productpercentage, 2) }}%</small>
+                                        @else
+                                            <small class="text-muted">{{ number_format($productpercentage, 2) }}%</small>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="border p-4">
+                                <small>Turnover</small>
+                                <h1 class="mb-0 mt-2">&dollar; {{ number_format($sales, 2, ',') }}</h1>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <small class="text-muted">&dollar; {{ number_format($oldSales, 2, ',') }}</small>
+                                    </div>
+                                    <div class="col-6 text-end">
+                                        @if ($salespercentage > 0)
+                                            <small class="text-success">{{ $salespercentage }}%</small>
+                                        @elseif($salespercentage < 0)
+                                            <small class="text-danger">{{ $salespercentage }}%</small>
+                                        @else
+                                            <small class="text-muted">{{ $salespercentage }}%</small>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -3,6 +3,9 @@
 namespace Varenyky\Http\Controllers;
 
 use Illuminate\View\View;
+use VarenykyECom\Models\Order;
+use VarenykyECom\Models\Product;
+use Carbon\Carbon;
 
 class DashboardController extends BaseController
 {
@@ -13,6 +16,46 @@ class DashboardController extends BaseController
 
     public function index(): View
     {
-        return view('varenyky::dashboard.index');
+        $isShop = false;
+
+        $orders = 0;
+        $oldOrders = 0;
+        $orderpercentage = 0;
+        $products = 0;
+        $oldProducts = 0;
+        $productpercentage = 0;
+        $sales = 0;
+        $oldSales = 0;
+        $salespercentage = 0;
+
+        if(class_exists(Order::class)){
+            $isShop = true;
+
+            $orders = Order::whereBetween('created_at', [Carbon::now()->startOfWeek()->format('Y-m-d H:i:s'), Carbon::now()->endOfWeek()->format('Y-m-d H:i:s')])->count();
+            $oldOrders = Order::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek()->format('Y-m-d H:i:s'), Carbon::now()->subWeek()->endOfWeek()->format('Y-m-d H:i:s')])->count();
+            if ($oldOrders != 0) {
+                $orderpercentage = (($orders - $oldOrders) / $oldOrders) * 100;
+            } else {
+                $orderpercentage = 0;
+            }
+
+            $products = Product::whereBetween('created_at', [Carbon::now()->startOfWeek()->format('Y-m-d H:i:s'), Carbon::now()->endOfWeek()->format('Y-m-d H:i:s')])->count();
+            $oldProducts = Product::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek()->format('Y-m-d H:i:s'), Carbon::now()->subWeek()->endOfWeek()->format('Y-m-d H:i:s')])->count();
+            if ($oldProducts != 0) {
+                $productpercentage = (($products - $oldProducts) / $oldProducts) * 100;
+            } else {
+                $productpercentage = 0;
+            }
+
+            $sales = Order::whereBetween('created_at', [Carbon::now()->startOfWeek()->format('Y-m-d H:i:s'), Carbon::now()->endOfWeek()->format('Y-m-d H:i:s')])->sum('total');
+            $oldSales = Order::whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek()->format('Y-m-d H:i:s'), Carbon::now()->subWeek()->endOfWeek()->format('Y-m-d H:i:s')])->sum('total');
+            if ($oldSales != 0) {
+                $salespercentage = (($sales - $oldSales) / $oldSales) * 100;
+            } else {
+                $salespercentage = 0;
+            }
+        }
+
+        return view('varenyky::dashboard.index', compact('isShop', 'sales', 'oldSales', 'salespercentage', 'orders', 'oldOrders', 'orderpercentage', 'products', 'oldProducts', 'productpercentage'));
     }
 }
